921a05665e559fafead9c15513b1a8bbec4af065
Nguyễn Duy Hưng <103039502+duyhung-it@users.noreply.github.com>
2023-10-05 21:51:23 +0700

    Feature/shoes category (#6)
    * #20232709 upload image
    
    * #20232709 upload image
    
    * #20232709 upload image
    
    * #20230510 create table Payment

---
+++ src/main/java/com/shoes/domain/Payment.java
@@ -0,0 +1,199 @@
+package com.shoes.domain;
+
+import java.io.Serializable;
+import java.time.Instant;
+import javax.persistence.*;
+
+/**
+ * A Payment.
+ */
+@Entity
+@Table(name = "payment")
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class Payment implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "payment_method")
+    private Integer paymentMethod;
+
+    @Column(name = "payment_status")
+    private Integer paymentStatus;
+
+    @Column(name = "status")
+    private Integer status;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_date")
+    private Instant createdDate;
+
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+    @Column(name = "last_modified_date")
+    private Instant lastModifiedDate;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public Payment id(Long id) {
+        this.setId(id);
+        return this;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCode() {
+        return this.code;
+    }
+
+    public Payment code(String code) {
+        this.setCode(code);
+        return this;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public Integer getPaymentMethod() {
+        return this.paymentMethod;
+    }
+
+    public Payment paymentMethod(Integer paymentMethod) {
+        this.setPaymentMethod(paymentMethod);
+        return this;
+    }
+
+    public void setPaymentMethod(Integer paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public Integer getPaymentStatus() {
+        return this.paymentStatus;
+    }
+
+    public Payment paymentStatus(Integer paymentStatus) {
+        this.setPaymentStatus(paymentStatus);
+        return this;
+    }
+
+    public void setPaymentStatus(Integer paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public Integer getStatus() {
+        return this.status;
+    }
+
+    public Payment status(Integer status) {
+        this.setStatus(status);
+        return this;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getCreatedBy() {
+        return this.createdBy;
+    }
+
+    public Payment createdBy(String createdBy) {
+        this.setCreatedBy(createdBy);
+        return this;
+    }
+
+    public void setCreatedBy(String createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Instant getCreatedDate() {
+        return this.createdDate;
+    }
+
+    public Payment createdDate(Instant createdDate) {
+        this.setCreatedDate(createdDate);
+        return this;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getLastModifiedBy() {
+        return this.lastModifiedBy;
+    }
+
+    public Payment lastModifiedBy(String lastModifiedBy) {
+        this.setLastModifiedBy(lastModifiedBy);
+        return this;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public Instant getLastModifiedDate() {
+        return this.lastModifiedDate;
+    }
+
+    public Payment lastModifiedDate(Instant lastModifiedDate) {
+        this.setLastModifiedDate(lastModifiedDate);
+        return this;
+    }
+
+    public void setLastModifiedDate(Instant lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Payment)) {
+            return false;
+        }
+        return id != null && id.equals(((Payment) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Payment{" +
+            "id=" + getId() +
+            ", code='" + getCode() + "'" +
+            ", paymentMethod=" + getPaymentMethod() +
+            ", paymentStatus=" + getPaymentStatus() +
+            ", status=" + getStatus() +
+            ", createdBy='" + getCreatedBy() + "'" +
+            ", createdDate='" + getCreatedDate() + "'" +
+            ", lastModifiedBy='" + getLastModifiedBy() + "'" +
+            ", lastModifiedDate='" + getLastModifiedDate() + "'" +
+            "}";
+    }
+}
