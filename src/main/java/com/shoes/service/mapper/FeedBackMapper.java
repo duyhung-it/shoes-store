930aa487c9e4e04f5d792e1842ad1ec3f364a9ee
hungnd2 <nguyenduyhung2003xx@gmail.com>
2023-11-21 16:08:47 +0700

    Add table feedback

---
+++ src/main/java/com/shoes/service/mapper/FeedBackMapper.java
@@ -0,0 +1,29 @@
+package com.shoes.service.mapper;
+
+import com.shoes.domain.FeedBack;
+import com.shoes.domain.Shoes;
+import com.shoes.domain.User;
+import com.shoes.service.dto.FeedBackDTO;
+import com.shoes.service.dto.ShoesDTO;
+import com.shoes.service.dto.UserDTO;
+import org.mapstruct.*;
+
+/**
+ * Mapper for the entity {@link FeedBack} and its DTO {@link FeedBackDTO}.
+ */
+@Mapper(componentModel = "spring")
+public interface FeedBackMapper extends EntityMapper<FeedBackDTO, FeedBack> {
+    @Mapping(target = "user", source = "user", qualifiedByName = "userId")
+    @Mapping(target = "shoes", source = "shoes", qualifiedByName = "shoesId")
+    FeedBackDTO toDto(FeedBack s);
+
+    @Named("userId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    UserDTO toDtoUserId(User user);
+
+    @Named("shoesId")
+    @BeanMapping(ignoreByDefault = true)
+    @Mapping(target = "id", source = "id")
+    ShoesDTO toDtoShoesId(Shoes shoes);
+}
