37a582fc426c35c8ea030dd8246eb76e396cb2de
quyetndph27146 <quyetndph27146@fpt.edu.vn>
2023-12-07 19:46:46 +0700

    giảm giá khi mua hàng

--- src/main/java/com/shoes/repository/CartDetailsRepository.java
+++ src/main/java/com/shoes/repository/CartDetailsRepository.java
@@ -20,13 +20,20 @@ public interface CartDetailsRepository extends JpaRepository<CartDetails, Long> 
     @Query(
         value = "select cd.*,fu.path,sd.price,s.name as nameshoes," +
         "sz.name as namesize,c.name as namecolor,sd.id as shoesdetailid,sd.quantity as quantityShoesDetail," +
-        "s.id as idsh,sz.id as idsz,c.id as idc,b.id as idb\n" +
+        "s.id as idsh,sz.id as idsz,c.id as idc,b.id as idb," +
+        "d.discount_method as discountmethod ,  " +
+        "dsd.discount_amount as discountamount_3_4 ,  " +
+        "d.discount_amount as discountamount_1_2\n" +
         "from cart_details cd\n" +
         "join shoes_details sd on cd.shoes_details_id = sd.id\n" +
         "join shoes s on  sd.shoes_id = s.id\n" +
         "join size sz on sd.size_id = sz.id\n" +
         "join color c on sd.color_id = c.id\n" +
         "join brand b on sd.brand_id = b.id\n" +
+        "left join discount_shoes_details dsd\n" +
+        "on dsd.shoes_details_id = sd.shoes_id and dsd.status = 1 and dsd.brand_id = b.id\n" +
+        "left join discount d \n" +
+        "on dsd.discount_id = d.id and d.start_date <= now() and d.end_date >= now() and d.status = 1\n" +
         "join (\n" +
         "\twith shoes_file_upload_mapping as(\n" +
         "\t\tselect * ,row_number() over(partition by shoes_details_id order by id) as rn\n" +
