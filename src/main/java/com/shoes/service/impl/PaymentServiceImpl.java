980b993165ee72cbd8af2c1a2b44da7962c1824b
lê dương <duongle5279@gmail.com>
2023-12-18 20:07:38 +0700

    address-order

--- src/main/java/com/shoes/service/impl/PaymentServiceImpl.java
+++ src/main/java/com/shoes/service/impl/PaymentServiceImpl.java
@@ -51,6 +51,7 @@ public class PaymentServiceImpl implements PaymentService {
     private final CartDetailsRepository cartDetailsRepository;
     private final MailService mailService;
     private final CartRepository cartRepository;
+    private final AddressRepository addressRepository;
 
     @Override
     public PaymentDTO save(PaymentDTO paymentDTO) {
@@ -110,6 +111,9 @@ public class PaymentServiceImpl implements PaymentService {
         String phone,
         String email,
         String address,
+        Integer province,
+        Integer district,
+        Integer ward,
         BigDecimal shipPrice,
         String idOwner,
         String arrSanPham,
@@ -148,6 +152,12 @@ public class PaymentServiceImpl implements PaymentService {
             email +
             "_" +
             address +
+            "_" +
+            province +
+            "_" +
+            district +
+            "_" +
+            ward +
             "_" +
             shipPrice +
             "_" +
@@ -243,10 +253,13 @@ public class PaymentServiceImpl implements PaymentService {
             String phone = orderInfoParts[1];
             String email = orderInfoParts[2];
             String address = orderInfoParts[3];
-            BigDecimal shipPrice = new BigDecimal(orderInfoParts[4]);
-            String arrSanPham = orderInfoParts[6];
-            String arrQuantity = orderInfoParts[7];
-            String idOwnerStr = orderInfoParts[5];
+            Integer province = Integer.valueOf(orderInfoParts[4]);
+            Integer district = Integer.valueOf(orderInfoParts[5]);
+            Integer ward = Integer.valueOf(orderInfoParts[6]);
+            BigDecimal shipPrice = new BigDecimal(orderInfoParts[7]);
+            String idOwnerStr = orderInfoParts[8];
+            String arrSanPham = orderInfoParts[9];
+            String arrQuantity = orderInfoParts[10];
             String[] sanPhamParts = arrSanPham.split("a");
             String[] quantityParts = arrQuantity.split("b");
             User owner;
@@ -267,6 +280,8 @@ public class PaymentServiceImpl implements PaymentService {
                 owner = null;
             }
 
+            Address adr = saveAddress(province, district, ward, address);
+
             Payment payment = new Payment();
             payment.setCode(orderCode);
             payment.setPaymentMethod(Constants.PAYMENT_METHOD.CREDIT);
@@ -275,7 +290,7 @@ public class PaymentServiceImpl implements PaymentService {
             payment.setCreatedDate(Instant.now());
             paymentRepository.save(payment);
 
-            Order order = newOrder(orderCode, address, phone, shipPrice, price, receivedBy, owner, email, payment);
+            Order order = newOrder(orderCode, phone, shipPrice, price, receivedBy, owner, email, payment, adr);
             List<OrderDetails> orderDetailsList = new ArrayList<>();
             ShoesDetails shoesDetails;
             OrderDetails orderDetails;
@@ -305,18 +320,18 @@ public class PaymentServiceImpl implements PaymentService {
 
     private Order newOrder(
         String orderCode,
-        String address,
         String phone,
         BigDecimal shipPrice,
         BigDecimal price,
         String receivedBy,
         User owner,
         String email,
-        Payment payment
+        Payment payment,
+        Address address
     ) {
         Order order = new Order();
         order.setCode(orderCode);
-        order.setAddress(address);
+        order.setUserAddress(address);
         order.setPhone(phone);
         order.setPaidMethod(Constants.PAYMENT_METHOD.CREDIT);
         order.setShipPrice(shipPrice);
@@ -329,5 +344,15 @@ public class PaymentServiceImpl implements PaymentService {
         order.setMailAddress(email);
         order.setPayment(payment);
         return order;
+    }
+
+    private Address saveAddress(Integer province, Integer district, Integer ward, String addressDetail) {
+        Address address = new Address();
+        address.setAddressDetails(addressDetail);
+        address.setProvince(province);
+        address.setDistrict(district);
+        address.setWard(ward);
+        addressRepository.save(address);
+        return address;
     }
 }
